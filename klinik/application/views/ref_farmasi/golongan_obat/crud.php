baa87d834394906ee99a6eac75ca185bf0e6980f
asepgunawanypd <asepgunawanypd@gmail.com>
2017-11-13 09:30:08 +0700

    Add files via upload

---
+++ klinik/application/views/ref_farmasi/golongan_obat/crud.php
@@ -0,0 +1,35 @@
+<div class="modal-header">
+  <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+  </button>
+  <h4 class="modal-title"></h4>
+</div>
+  <div class="modal-body">
+    <div class="row">
+		<div class="col-md-12">
+        <form id="form" class="form-horizontal form-label-left" enctype="multipart/form-data">
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="kd">Kode <span class="required">*</span></label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input type="hidden" name="created_at" id="created_at">
+              <input type="hidden" name="created_by" id="created_by">
+              <input type="text" id="kd" name="kd" required="required" autocomplete="off" class="form-control col-md-8 col-xs-12"><div id="msg_kd"></div>
+            </div>
+			     </div>  
+           <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="golongan_obat">Golongan Obat <span class="required">*</span></label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+               <input type="text" id="golongan_obat" name="golongan_obat" required="required" autocomplete="off" class="form-control col-md-8 col-xs-12">
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal-footer" style="text-align: center">
+      <button class="btn btn-warning" type="button" data-dismiss="modal"><i class="fa fa-close"></i> Cancel</button>
+      <button class="btn btn-info" type="reset"><i class="glyphicon glyphicon-refresh"></i> Reset</button>
+      <button type="submit" class="btn btn-success"><i class="glyphicon glyphicon-saved"></i> Save</button>
+  </div>
+</form>
+
+
+
---
+++ klinik/application/views/ref_farmasi/golongan_obat/view.php
@@ -0,0 +1,20 @@
+<div class="modal-header">
+  <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+  </button>
+  <h4 class="modal-title">Detail</h4>
+</div>
+  <div class="modal-body">
+    <div class="row">
+      <div class="col-md-12">
+        <table class="table table-striped">
+          <tbody>
+            <tr><th width="20%">Kode</th><td id="v_kd"></td></tr>
+            <tr><th>Golongan Obat</th><td id="v_golongan_obat"></td></tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+<div class="modal-footer">
+  <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+</div>
