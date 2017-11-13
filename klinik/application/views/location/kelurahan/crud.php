a29b8052ada955925032b0ce403ecc1de0c9a7fe
asepgunawanypd <asepgunawanypd@gmail.com>
2017-11-13 09:19:18 +0700

    Add files via upload

---
+++ klinik/application/views/location/kelurahan/crud.php
@@ -0,0 +1,53 @@
+<div class="modal-header">
+  <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+  </button>
+  <h4 class="modal-title"></h4>
+</div>
+  <div class="modal-body">
+    <div class="row">
+		<div class="col-md-12">
+        <form id="form" class="form-horizontal form-label-left" enctype="multipart/form-data">
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="id">ID <span class="required">*</span></label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+				<input type="hidden" name="created_at" id="created_at">
+                <input type="hidden" name="created_by" id="created_by">
+				<input type="number" id="id" name="id" autofocus="autofocus" required="required" autocomplete="off" class="form-control col-md-8 col-xs-12"><div id="msg_username"></div>
+				<div id="msg_id"></div>
+			</div> 
+          </div>
+		  <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="kecamatan_id">Kecamatan <span class="required">*</span></label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <select id="kecamatan_id" required name="kecamatan_id" class="form-control col-md-8 col-xs-12" >
+                <option></option>
+                 <?php
+                   foreach ($dataSelect as $val) {
+                     ?>
+                     <option value="<?php echo $val->id; ?>">
+                       <?php echo $val->kecamatan; ?>
+                     </option>
+                     <?php
+                   }
+                 ?>
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="Kelurahan">Kelurahan <span class="required">*</span></label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input type="text" id="kelurahan" name="kelurahan" required="required" autocomplete="off" class="form-control col-md-8 col-xs-12">
+            </div>
+			</div>  
+        </div>
+    </div>
+  </div>
+  <div class="modal-footer" style="text-align: center">
+      <button class="btn btn-warning" type="button" data-dismiss="modal"><i class="fa fa-close"></i> Cancel</button>
+      <button class="btn btn-info" type="reset"><i class="glyphicon glyphicon-refresh"></i> Reset</button>
+      <button type="submit" class="btn btn-success"><i class="glyphicon glyphicon-saved"></i> Save</button>
+  </div>
+</form>
+
+
+
---
+++ klinik/application/views/location/kelurahan/view.php
@@ -0,0 +1,22 @@
+<div class="modal-header">
+  <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+  </button>
+  <h4 class="modal-title">Detail</h4>
+</div>
+  <div class="modal-body">
+    <div class="row">
+      <div class="col-md-12">
+        <table class="table table-striped">
+          <tbody>
+            <tr><th width="20%">ID Kelurahan</th><td id="v_id"></td></tr>
+            <tr><th>ID Kecamatan</th><td id="v_kecamatan_id"></td></tr>
+            <tr><th>Kecamatan</th><td id="v_kecamatan"></td></tr>
+            <tr><th>Kelurahan</th><td id="v_kelurahan"></td></tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+<div class="modal-footer">
+  <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+</div>
