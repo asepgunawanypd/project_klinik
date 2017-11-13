26d879818123f908c0a965af716b415045af2915
asepgunawanypd <asepgunawanypd@gmail.com>
2017-11-13 09:18:06 +0700

    Add files via upload

---
+++ klinik/application/views/location/kabupaten/view.php
@@ -0,0 +1,22 @@
+<div class="modal-header">
+  <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+  </button>
+  <h4 class="modal-title">Detail</h4>
+</div>
+  <div class="modal-body">
+    <div class="row">
+      <div class="col-md-12">
+        <table class="table table-striped">
+          <tbody>
+            <tr><th width="20%">ID</th><td id="v_id"></td></tr>
+            <tr><th>ID Provinsi</th><td id="v_provinsi_id"></td></tr>
+            <tr><th>Provinsi</th><td id="v_provinsi"></td></tr>
+            <tr><th>Kabupaten / Kota</th><td id="v_kabupaten"></td></tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+<div class="modal-footer">
+  <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+</div>
