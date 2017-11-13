cf2a18cf184c2aa02a50154b83f6c3d242ef40fe
asepgunawanypd <asepgunawanypd@gmail.com>
2017-11-13 09:19:49 +0700

    Add files via upload

--- klinik/application/views/location/provinsi/main_provinsi.php
+++ klinik/application/views/location/provinsi/main_provinsi.php
@@ -1,16 +1,16 @@
 <div class="" role="tabpanel" data-example-id="togglable-tabs">
   <ul id="myTab" class="nav nav-tabs bar_tabs" role="tablist">
-    <li class="active"><a href="<?php echo base_url('Provinsi'); ?>" id="pro-tab">Province</a></li>
-    <li class=""><a href="<?php echo base_url('Kabupaten'); ?>" id="kab-tab">District / City</a></li>
-    <li class=""><a href="<?php echo base_url('Kecamatan'); ?>" id="kec-tab">Districts</a></li>
-    <li class=""><a href="<?php echo base_url('Kelurahan'); ?>" id="kel-tab">Village</a></li>
+    <li class="active"><a href="<?php echo base_url('Provinsi'); ?>" id="pro-tab">Provinsi</a></li>
+    <li class=""><a href="<?php echo base_url('Kabupaten'); ?>" id="kab-tab">Kabupaten / Kota</a></li>
+    <li class=""><a href="<?php echo base_url('Kecamatan'); ?>" id="kec-tab">Kecamatan</a></li>
+    <li class=""><a href="<?php echo base_url('Kelurahan'); ?>" id="kel-tab">Kelurahan</a></li>
   </ul>
   <div id="myTabContent" class="tab-content">
     <div role="tabpanel" class="tab-pane fade active in" id="tab_content1" aria-labelledby="pro-tab">
       <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
           <div class="x_title">
-            <h2>List Province<small>Management</small></h2>
+            <h2>List Provinsi<small>Management</small></h2>
             <ul class="nav navbar-right panel_toolbox">        
               <li><a class="collapse-link"><i class="fa fa-circle-o"></i></a></li>
               <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
@@ -25,7 +25,7 @@
                 <tr>
                   <th style="width: 5%;"><div align="center">No</div></th>
                   <th style="width: 10%;"><div align="center">ID</div></th>
-                  <th><div align="center">Province</div></th>
+                  <th><div align="center">Provinsi</div></th>
                 </tr>
               </thead>
             </table>
@@ -39,9 +39,9 @@
                   <?php } if($access->location_d != 0){ ?>
                   <button id="delete" type="button" class="btn btn-danger"><i class="glyphicon glyphicon-trash"></i> Delete</button>
                   <?php } if($access->location_p != 0){ ?>
-                  <a href="<?php echo base_url('User/printpdf'); ?>" target="_blank" class="btn btn-info" title="Print"><i class="glyphicon glyphicon-print"></i> Print</a>
+                  <a href="<?php echo base_url('Provinsi/printpdf'); ?>" target="_blank" class="btn btn-info" title="Print"><i class="glyphicon glyphicon-print"></i> Print</a>
                   <?php } if($access->location_ex != 0){ ?>
-                  <a href="<?php echo base_url('User/export'); ?>" class="btn btn-info"><i class="glyphicon glyphicon glyphicon-floppy-save"></i> Export Excel</a>
+                  <a href="<?php echo base_url('Provinsi/export'); ?>" class="btn btn-info"><i class="glyphicon glyphicon glyphicon-floppy-save"></i> Export Excel</a>
                   <?php } ?>
               </div>
           </div>
@@ -51,74 +51,178 @@
   </div>
 </div>
 <?php echo $modal_view; ?>
-
+<?php echo $modal_crud; ?>
 <script type="text/javascript">
-  $(document).ready(function(){ 
-    $.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings){
-        return {
-            "iStart": oSettings._iDisplayStart,
-            "iEnd": oSettings.fnDisplayEnd(),
-            "iLength": oSettings._iDisplayLength,
-            "iTotal": oSettings.fnRecordsTotal(),
-            "iFilteredTotal": oSettings.fnRecordsDisplay(),
-            "iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
-            "iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
-        };
-    };
-    // ========= DataTable =============
-    var table = $('#tableData').DataTable({ 
-        processing: true, 
-        serverSide: true,
-        aLengthMenu: [5,10, 25, 50, 100],  
-        ajax: {
-            url: "<?php echo base_url('Provinsi/dataTable')?>",
-            type: "POST"
-        },
-        language: {
-          infoFiltered: " "
-        },
-        order: [],
-        columns: [
-            { "data": null },
-            { "data": "id",
-               fnCreatedCell: function (nTd, sData, oData, iRow, iCol) {
-                $(nTd).html('<div align="center">'+oData.id+'</div>');
-               }
-            },
-            { "data": "provinsi"}
-        ], 
-        rowCallback: function(row, data, iDisplayIndex) {
-            var info = this.fnPagingInfo();
-            var page = info.iPage;
-            var length = info.iLength;
-            var index = page * length + (iDisplayIndex + 1);
-            $('td:eq(0)', row).html('<div align="center">'+index+'</div>');
-        },
-        columnDefs: [
-          { "orderable": false, "targets": 0 }
-        ]
-    });
-    // ========= Select Row =============
-    $('#tableData tbody').on( 'click', 'tr', function () {
-      if ( $(this).hasClass('selected') ) {
-          $(this).removeClass('selected');
-      }
-      else {
-          table.$('tr.selected').removeClass('selected');
-          $(this).addClass('selected');
-      }
-    });
-     $('#view').click( function (){
-        var row = $('#tableData').DataTable().row('.selected').data();
-        if( ! row ){ swal("Warning", "No data selected", "info"); }
-        else{
-          $.post("<?php echo base_url('Provinsi/view'); ?>", { id:row.id  }, function(data) {
-            var obj = JSON.parse(data);
-            $('#modal_view').modal({show:true});
-            $('#v_id').text(': '+obj.id);
-            $('#v_provinsi').text(': '+obj.provinsi);
-          });
-        }
-    });
-  });
+	function add(){
+		save_method = 'add';
+		$('.modal-title').text('Tambah Provinsi');
+		$('#form')[0].reset();
+		$('#modal_crud').modal({show:true});
+    }
+	$(document).ready(function(){ 
+		$.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings){
+			return {
+				"iStart": oSettings._iDisplayStart,
+				"iEnd": oSettings.fnDisplayEnd(),
+				"iLength": oSettings._iDisplayLength,
+				"iTotal": oSettings.fnRecordsTotal(),
+				"iFilteredTotal": oSettings.fnRecordsDisplay(),
+				"iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
+				"iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
+			};
+		};
+		// ========= DataTable =============
+		var table = $('#tableData').DataTable({ 
+			processing: true, 
+			serverSide: true,
+			aLengthMenu: [5,10, 25, 50, 100],  
+			ajax: {
+				url: "<?php echo base_url('Provinsi/dataTable')?>",
+				type: "POST"
+			},
+			language: {
+			  infoFiltered: " "
+			},
+			order: [],
+			columns: [
+				{ "data": null },
+				{ "data": "id",
+				   fnCreatedCell: function (nTd, sData, oData, iRow, iCol) {
+					$(nTd).html('<div align="center">'+oData.id+'</div>');
+				   }
+				},
+				{ "data": "provinsi"}
+			], 
+			rowCallback: function(row, data, iDisplayIndex) {
+				var info = this.fnPagingInfo();
+				var page = info.iPage;
+				var length = info.iLength;
+				var index = page * length + (iDisplayIndex + 1);
+				$('td:eq(0)', row).html('<div align="center">'+index+'</div>');
+			},
+			columnDefs: [
+			  { "orderable": false, "targets": 0 }
+			]
+		});
+		// ========= Select Row =============
+		$('#tableData tbody').on( 'click', 'tr', function () {
+		  if ( $(this).hasClass('selected') ) {
+			  $(this).removeClass('selected');
+		  }
+		  else {
+			  table.$('tr.selected').removeClass('selected');
+			  $(this).addClass('selected');
+		  }
+		});
+		 $('#view').click( function (){
+			var row = $('#tableData').DataTable().row('.selected').data();
+			if( ! row ){ swal("Warning", "No data selected", "info"); }
+			else{
+			  $.post("<?php echo base_url('Provinsi/view'); ?>", { id:row.id  }, function(data) {
+				var obj = JSON.parse(data);
+				$('#modal_view').modal({show:true});
+				$('#v_id').text(': '+obj.id);
+				$('#v_provinsi').text(': '+obj.provinsi);
+			  });
+			}
+		});
+		$( "#id" ).on('keyup', function( event ) {
+			event.preventDefault();
+			$("#msg_id").html('<img src="<?php echo base_url('assets/img/loader.gif')?>" align="absmiddle">&nbsp;Checking availability...');
+			var id = $(this).val();
+			$.post("<?php echo base_url('Provinsi/filename_exists'); ?>", { id:id  }, function(data) {   
+				if (data == 'error') {
+					$('#msg_id').html('<span style="color:red;">Value does not exist</span>');
+				}else{
+					$('#msg_id').html('');
+				}
+			window.globalVar = data;
+			});
+		});
+		$('#form').submit(function(e){
+			e.preventDefault(); 
+			var url;
+			var cek;
+			if(save_method == 'add'){
+			   url = "<?php echo site_url('Provinsi/add')?>";
+			   cek = globalVar;
+			}else{
+			   url = "<?php echo site_url('Provinsi/update')?>";
+			   cek = 'ok';
+			}
+			if(cek == 'error'){
+			   swal("Warning", "ID fileid", "warning");
+			}else{
+				$.ajax({
+					url : url,
+					type: "POST",
+					data: new FormData(this),
+					processData:false,
+					contentType:false,
+					cache:false,
+					dataType: "JSON",
+					success: function(data){
+						$('#modal_form').modal('hide');
+						swal('Success','Data Seved', 'success').then(function(){
+							location.reload();
+						}); 
+					},
+					error: function (data){
+						swal("Insert/Update  Data Error", "", "info").then(function(){
+							location.reload();
+						}); 
+					}
+				});
+			}
+		});
+		$('#edit').click(function(){
+            save_method = 'update';
+            var data = $('#tableData').DataTable().row('.selected').data();
+            if( ! data ){ swal("No data selected", "", "info"); }
+            else{
+				$.post("<?php echo base_url('Provinsi/edit_view'); ?>", { id:data.id  }, function(data) {
+					var obj = JSON.parse(data);
+					$('[name="id"]').val(obj.id);
+					$('[name="id"]').attr('readonly', true);
+					$('[name="provinsi"]').val(obj.provinsi);
+					$('[name="created_by"]').val(obj.created_by);
+          			$('[name="created_at"]').val(obj.created_at);
+					$('#modal_crud').modal({show:true});
+					$('.modal-title').text('Edit Provinsi');
+					window.id = obj.id;
+				});
+			}
+		});
+		$('#delete').click(function(){
+            var data = $('#tableData').DataTable().row('.selected').data();
+            if( ! data ){ swal("No data selected", "", "info"); }
+            else{
+               swal({
+                   title: 'Delete File Attachment',
+                   text: 'Are You Sure Delete This File?',
+                   type: 'warning',
+                   html: '',
+                   confirmButtonColor: '#d9534f',
+                   confirmButtonText: "Yes, delete",
+                   showCloseButton: true,
+                   showCancelButton: true,
+                 }).then(function(){
+                 $.ajax({
+                   url : "<?php echo site_url('Provinsi/delete')?>/"+data.id,
+                   type: "POST",
+                   success: function(data){               
+                     swal('success','data success delete','success').then(function(){
+                       location.reload();
+                     }); 
+                   },
+                   error: function (data){
+                     swal('Error','data is ready not delete ','error').then(function(){
+                        location.reload();
+                     })
+                   },
+                 });
+               });
+            }
+         });
+	});
 </script>
---
+++ klinik/application/views/location/provinsi/print.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $judul; ?></title>
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url('assets/vendors/bootstrap/dist/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+   <!-- Bootstrap -->
+    <script src="<?php echo base_url('assets/vendors/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
+</head>
+<body>
+    <table  width="100%" border="0" cellspacing="5" cellpadding="5">
+      <tr>
+        <td align="center"><strong><h3><?php echo $judul; ?></h3></strong></td>
+      </tr>
+    </table>
+    <p></p>
+    <table class="table table-bordered dt-responsive nowrap" width="100%" border="1" cellspacing="5" cellpadding="5">>
+        <thead> 
+            <tr>   
+                <th style="padding: 5px;" align="center">No</th>
+                <th style="padding: 5px;" align="center">ID</th>
+                <th style="padding: 5px;" align="center">Provinsi</th>         
+            </tr>    
+        </thead>
+        <tbody> 
+            <?php
+			$no = 1;
+            foreach ($data_list as $val) {
+                ?>
+                <tr>
+					<td style="padding: 5px;" align="center"><?php echo $no; ?></td>
+                    <td style="padding: 5px;" ><?php echo $val->id; ?></td>
+                    <td style="padding: 5px;"><?php echo $val->provinsi; ?></td>
+                </tr>
+				
+            <?php $no++; } ?> 
+        </tbody>
+    </table>
+</body>
+</html>
